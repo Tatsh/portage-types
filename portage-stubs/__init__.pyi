2e57029ce81985272577a09d49b28a14f786a085
INODE64 <web@inode64.com>
2025-01-30 11:38:52 +0100

    more typed

--- portage-stubs/__init__.pyi
+++ portage-stubs/__init__.pyi
@@ -1,8 +1,8 @@
 # pylint: disable=too-few-public-methods,unused-argument
-from typing import Any, Literal, Mapping, TypedDict
+from typing import Any, Literal, Mapping, TypedDict, Union, Optional
 from collections.abc import Sequence
 
-__all__ = ('db', 'root')
+__all__ = ('db', 'root', 'settings')
 
 
 class dbapi:
@@ -49,23 +49,53 @@ class DBRootDict(TypedDict):
     virtuals: Any
 
 
-def doebuild(myebuild, mydo, _unused=...,
-        settings: Incomplete | None = None,
-        debug: int = 0,
-        listonly: int = 0,
-        fetchonly: int = 0,
-        cleanup: int = 0,
-        use_cache: int = 1,
-        fetchall: int = 0,
-        tree: Incomplete | None = None,
-        mydbapi: Incomplete | None = None,
-        vartree: Incomplete | None = None,
-        prev_mtimes: Incomplete | None = None,
-        fd_pipes: Incomplete | None = None,
-        returnpid: bool = False,
-        returnproc: bool = False) -> int | portage.process.MultiprocessingProcess | list[int]:
-    ...
+class config:
+    def __init__(
+        self,
+        clone: Optional["config"] = None,
+        mycpv: Optional[str] = None,
+        config_profile_path: Optional[str] = None,
+        config_incrementals: Optional[dict[str, str]] = None,
+        config_root: Optional[str] = None,
+        target_root: Optional[str] = None,
+        sysroot: Optional[str] = None,
+        eprefix: Optional[str] = None,
+        local_config: bool = True,
+        env: Optional[dict[str, str]] = None,
+        _unmatched_removal: bool = False,
+        repositories: Optional[list[str]] = None,
+    ) -> None: ...
+    def __getitem__(self, key: str) -> str: ...
+    def __setitem__(self, key: str, value: str) -> None: ...
+    def __delitem__(self, key: str) -> None: ...
+    def __iter__(self) -> str: ...
+    def get(self, k: str, x: Optional[Any] = None) -> Optional[Any]: ...
+    def __len__(self) -> int: ...
+
+
+def doebuild(
+    myebuild:str,
+    mydo:Literal['info', 'nofetch', 'pretend', 'configure', 'compile', 'merge',
+                 'test','install','prepare','clean','cleanrm','depend', 'digest',
+                 'fetch','fetchall','help','manifest','qmerge','postinst','postrm',
+                 'preinst','prepare','prerm','setup']= ...,
+    settings:Optional[str]=None,
+    debug:Literal[0, 1]=0,
+    listonly:Literal[0, 1]=0,
+    fetchonly:Literal[0, 1]=0,
+    cleanup:Literal[0, 1]=0,
+    use_cache:Literal[0, 1]=1,
+    fetchall:Literal[0, 1]=0,
+    tree:Literal['vartree','porttree','bintree']='porttree',
+    mydbapi:Optional[str]=None,
+    vartree:Optional[str]=None,
+    prev_mtimes:Optional[str]=None,
+    fd_pipes:Optional[str]=None,
+    returnpid:Optional[int]=False,
+    returnproc:Optional[int]=False,
+) -> Union[int, str, list[int]]: ...
 
 
 db: Mapping[str, DBRootDict]
 root: str
+settings: config
